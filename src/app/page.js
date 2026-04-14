9f6e368b4ae9cf7404fd14bb8cd8c0786d2df6f7
KhandakarShakilRana <khandakarshakilrana770@gmail.com>
2026-04-14 16:29:33 +0600

    banner added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Banner from "@/components/banner/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="max-w-300 w-full mx-auto py-5">
-    This is home
+    <Banner/>
     </div>
   );
 }
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { CiSquarePlus } from "react-icons/ci";
+
+const Banner = () => {
+  return (
+    <div className="w-full text-center ">
+      <h1 className="text-[48px] text-[#244D3F] font-bold mb-4">Friends to keep close in your life</h1>
+      <p className="text-[#64748B] text-[16px] mb-8">
+        Your personal shelf of meaningful connections. Browse, tend, and nurture
+        the <br /> relationships that matter most.
+      </p>
+      <button className="flex justify-center items-center mx-auto gap-3 bg-[#244D3F] px-4 py-2 rounded-xl text-white font-semibold"><CiSquarePlus />Add a Friend</button>
+    </div>
+  );
+};
+
+export default Banner;
